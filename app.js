840fca5b6b32134b38e045f1f81419eb90e5ec99
Mohd Joun <mohammad.joun@nviz.com>
2017-05-16 15:47:47 +0530

    UpdaTING APP.JS

--- app.js
+++ app.js
@@ -31,11 +31,13 @@ var dbHost = process.env.DB_HOST || 'localhost'
 var dbPort = process.env.DB_PORT || 27017;
 var dbName = process.env.DB_NAME || 'node-login';
 
-var dbURL = 'mongodb://'+dbHost+':'+dbPort+'/'+dbName;
+/*var dbURL = 'mongodb://'+dbHost+':'+dbPort+'/'+dbName;
 if (app.get('env') == 'live'){
 // prepend url with authentication credentials // 
 	dbURL = 'mongodb://'+process.env.DB_USER+':'+process.env.DB_PASS+'@'+dbHost+':'+dbPort+'/'+dbName;
-}
+}*/
+
+var dbURL = process.env.MONGODB_URI
 
 app.use(session({
 	secret: 'faeb4453e5d14fe6f6d04637f78077c76c73d1b4',
